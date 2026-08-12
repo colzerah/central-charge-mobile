51447c34ecc2b1e8962c7785a50fefb9bdb364c6
Dyego Lima <dyegotavares@gmail.com>
2026-08-11 23:17:44 -0300

    add conhecimentos de rotas react native expo

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -1,13 +1,13 @@
-import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
+import { DarkTheme, DefaultTheme, Tabs, ThemeProvider } from "expo-router";
 import { useColorScheme } from "react-native";
 
-export default function RootLayout() {
+export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack
+      <Tabs
         screenOptions={{
-          headerShown: false,
+          headerShown: true,
         }}
       />
     </ThemeProvider>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,22 @@
-import { StyleSheet, View, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRouter } from "expo-router";
 
+import { Button, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const router = useRouter();
+
+  function handleNavigate() {
+    console.log("Navigating to login page");
+    router.navigate("/Login");
+  }
   return (
     <View style={styles.container}>
       <SafeAreaView>
         <View style={styles.heroSection}>
-          <Text style={styles.title}>
-            Hello World!
-          </Text>
+          <Text style={styles.title}>Hello World!</Text>
+          <Button onPress={handleNavigate} title="Go to Login" />
+          <Button onPress={() => router.navigate("/Home")} title="Go to Home" />
         </View>
       </SafeAreaView>
     </View>
@@ -19,15 +26,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    flexDirection: 'row',
+    justifyContent: "center",
+    flexDirection: "row",
   },
   heroSection: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     flex: 1,
   },
   title: {
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
