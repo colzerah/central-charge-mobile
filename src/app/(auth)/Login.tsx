fe6eebf68887c2204446087b0ae1eaa5f2b72deb
Dyego Lima <dyegotavares@gmail.com>
2026-08-14 00:45:47 -0300

    add maps

--- src/app/(auth)/Login.tsx
+++ src/app/(auth)/Login.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
 
 export default function Login() {
   return (
-    <View>
-      <Text>Central Charge Login</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_DEFAULT}
+        style={styles.map}
+        initialRegion={{
+          latitude: 20405060,
+          longitude: 20405060,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "90%",
+  },
+});
